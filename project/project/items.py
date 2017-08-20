66bedfa49868a5560e37835611b8631ca507b1c5
jpovmarques <jpovmarques@gmail.com>
2017-08-20 17:08:59 +0100

    Optimize standvirtual spider

--- project/project/items.py
+++ project/project/items.py
@@ -2,5 +2,6 @@ from scrapy.item import Item, Field
 
 
 class StandVirtualItem(Item):
+    link = Field()
     title = Field()
     price = Field()
--- project/project/spiders/stand_virtual_spider.py
+++ project/project/spiders/stand_virtual_spider.py
@@ -14,9 +14,22 @@ class StandVirtualSpider(Spider):
         ads = Selector(response).xpath('//div[@class="offer-item__content"]')
         for ad in ads:
             item = StandVirtualItem()
+            item['link'] = ad.xpath(
+                '''div[@class="offer-item__title"]/
+                h2[@class="offer-title"]/
+                a[@class="offer-title__link"]/
+                @href'''
+            ).extract()[0]
             item['title'] = ad.xpath(
                 '''div[@class="offer-item__title"]/
                 h2[@class="offer-title"]/
-                a[@class="offer-title__link"]/text()'''
+                a[@class="offer-title__link"]/
+                @title'''
             ).extract()[0]
+            item['price'] = ad.xpath(
+                '''div[@class="offer-item__price"]/
+                div[@class="offer-price"]/
+                span[@class="offer-price__number"]/
+                text()'''
+            ).extract()[0].rstrip()
             yield item
