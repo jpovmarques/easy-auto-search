a451040b876593cb2dd8c2bd9011dfd231373690
jpovmarques <jpovmarques@gmail.com>
2017-08-23 20:51:27 +0100

    Update spider for scrapping power and gas_type

--- project/project/items.py
+++ project/project/items.py
@@ -2,7 +2,15 @@ from scrapy.item import Item, Field
 
 
 class StandVirtualItem(Item):
+    brand = Field()  # TODO
+    model = Field()  # TODO
+    gas_type = Field()
+    power = Field()  # TODO
+    lotation = Field()  # TODO
+    year = Field()  # TODO
+    kms = Field()  # TODO
+    price = Field()
+    picture = Field()
     link = Field()
     title = Field()
-    price = Field()
-    photo = Field()
+    location = Field()  # TODO
--- project/project/spiders/stand_virtual_spider.py
+++ project/project/spiders/stand_virtual_spider.py
@@ -11,9 +11,11 @@ class StandVirtualSpider(Spider):
 
     def start_requests(self):
         url = (
-            'https://www.standvirtual.com/carros/{}/?search%5Bnew_used%5D=on'
-            .format(self.brand)
+            'https://www.standvirtual.com/'
+            'carros/{brand}/?search%5Bnew_used%5D=on'
+            .format(brand=self.brand)
         )
+        print('url, url')
         yield scrapy.Request(url)
 
     def parse(self, response):
@@ -23,6 +25,30 @@ class StandVirtualSpider(Spider):
 
         for ad in ads:
             item = StandVirtualItem()
+
+            item['gas_type'] = ad.xpath(
+                '''div[@class="offer-item__content"]/
+                ul[@class="offer-item__params"]/
+                li[@data-code="fuel_type"]/
+                span/
+                text()'''
+            ).extract()[0]
+
+            item['power'] = ad.xpath(
+                '''div[@class="offer-item__content"]/
+                ul[@class="offer-item__params"]/
+                li[@data-code="power"]/
+                span/
+                text()'''
+            ).extract()[0]
+
+            item['year'] = ad.xpath(
+                '''div[@class="offer-item__content"]/
+                ul[@class="offer-item__params"]/
+                li[@data-code="first_registration_year"]/
+                span/
+                text()'''
+            ).extract()[0]
 
             item['link'] = ad.xpath(
                 '''div[@class="offer-item__content"]/
@@ -48,11 +74,15 @@ class StandVirtualSpider(Spider):
                 text()'''
             ).extract()[0].rstrip()
 
-            photo = ad.xpath(
-                ''' div[@class="offer-item__photo "]/
+            picture = ad.xpath(
+                '''div[@class="offer-item__photo "]/
                 a[@class="offer-item__photo-link"]/
                 @style''').extract_first()
 
-            item['photo'] = re.search(r"'.*'", photo).group(0).split("'")[1]
+            # TODO: Make regex more restrict
+            item['picture'] = re.search(
+                r"'.*'",
+                picture
+            ).group(0).split("'")[1]
 
             yield item
