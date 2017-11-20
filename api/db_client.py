0ee3b83b18747737e9dd262086141b8c2f736e04
João Marques <jpm@advert.io>
2017-11-20 23:56:29 +0000

    add pagination and model query params

--- api/db_client.py
+++ api/db_client.py
@@ -1,9 +1,13 @@
+import json
 from pymongo import MongoClient
 import settings
 
 
 class Database_client(object):
     def __init__(self):
+        """
+        create a mongodb client instance.
+        """
         client = MongoClient(
             settings.MONGODB_SERVER,
             settings.MONGODB_PORT
@@ -12,14 +16,30 @@ class Database_client(object):
         self.collection = db[settings.MONGODB_COLLECTION]
 
     def count(self):
+        """
+        returns the total number of documents.
+        """
         return {'count': self.collection.count()}
 
     def all(self):
-        data = {}
-        documentet_list = []
-        for document in self.collection.find():
-            document['_id'] = str(document['_id'])
-            documentet_list.append(document)
+        """
+        returns all documents.
+        """
+        cursor = self.collection.find()
+        return {'all_cars_list': [str(x) for x in cursor]}
 
-        data['all_cars_list'] = documentet_list;
-        return data
+    def search(self, page_size, page_num, brand=None):
+        """
+        returns a set of documents belonging to page number `page_num`
+        where size of each page is `page_size`.
+        """
+        skips = page_size * (page_num - 1)
+        if not page_size or not page_num:
+            return {'error': 'page size and page number are required params'}
+
+        elif brand:
+            cursor = self.collection.find({"brand": brand}).skip(skips).limit(page_size)
+            return {'results': [str(x) for x in cursor if x['_id']]}
+
+        cursor = self.collection.find().skip(skips).limit(page_size)
+        return {'results': [str(x) for x in cursor if x['_id']]}
--- api/server.py
+++ api/server.py
@@ -1,16 +1,20 @@
 import time
-from bottle import route, run
+from bottle import route, run, request
 from db_client import Database_client
 
 
 class Server:
     db_client = Database_client()
     def __init__(self):
-        run(host='localhost', port=1111)
+        run(host='localhost', port=1111, reloader=True)
 
     @route('/')
     def index():
         return {'path': 'home'}
+
+    @route('/api/status')
+    def api_status():
+        return {'status': 'online', 'servertime': time.time()}
 
     @route('/api/count')
     def count():
@@ -20,6 +24,9 @@ class Server:
     def all():
         return Server.db_client.all()
 
-    @route('/api/status')
-    def api_status():
-    	return {'status': 'online', 'servertime': time.time()}
+    @route('/api/search')
+    def all_with_pagination():
+        page_size = int(request.query.size)
+        page_number = int(request.query.number)
+        brand = request.query.brand or None
+        return Server.db_client.search(page_size, page_number, brand)
